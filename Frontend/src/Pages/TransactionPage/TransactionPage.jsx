cab3da78346b679f0f0942924085076e79046b7f
SohailShafiq1 <sohail.shafiq002@gmail.com>
2025-04-27 00:07:53 +0500

    responsive added

--- Frontend/src/Pages/TransactionPage/TransactionPage.jsx
+++ Frontend/src/Pages/TransactionPage/TransactionPage.jsx
@@ -1,16 +1,16 @@
-import { AiOutlineCheckCircle } from "react-icons/ai"; 
-import React from 'react';
-import styles from './TransactionPage.module.css';
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import React from "react";
+import styles from "./TransactionPage.module.css";
 const s = styles;
 
 const TransactionPage = () => {
   const transactions = [
-    { order: 456985, date: '16/03/2025', status: 'Processing', type: 'Deposit', payment: 'USD Tether (BEP-20)', amount: '+$90.00' },
-    { order: 456947, date: '27/00/2025', status: 'Successed', type: 'Deposit', payment: 'USD Tether (TRC-20)', amount: '+$20.00' },
-    { order: 456964, date: '19/02/2025', status: 'Successed', type: 'Deposit', payment: 'Bitcoin (BTC)', amount: '+$40.00' },
-    { order: 456944, date: '05/02/2025', status: 'Successed', type: 'Deposit', payment: 'USD Tether (BEP-20)', amount: '+$50.00' },
-    { order: 456925, date: '28/01/2025', status: 'Successed', type: 'Deposit', payment: 'USD Tether (TRC-20)', amount: '+$20.00' },
-    { order: 456949, date: '14/01/2025', status: 'Successed', type: 'Deposit', payment: 'Bitcoin (BTC)', amount: '+$10.00' },
+    { order: 456985, date: "16/03/2025", status: "Processing", type: "Deposit", payment: "USD Tether (BEP-20)", amount: "+$90.00" },
+    { order: 456947, date: "27/00/2025", status: "Successed", type: "Deposit", payment: "USD Tether (TRC-20)", amount: "+$20.00" },
+    { order: 456964, date: "19/02/2025", status: "Successed", type: "Deposit", payment: "Bitcoin (BTC)", amount: "+$40.00" },
+    { order: 456944, date: "05/02/2025", status: "Successed", type: "Deposit", payment: "USD Tether (BEP-20)", amount: "+$50.00" },
+    { order: 456925, date: "28/01/2025", status: "Successed", type: "Deposit", payment: "USD Tether (TRC-20)", amount: "+$20.00" },
+    { order: 456949, date: "14/01/2025", status: "Successed", type: "Deposit", payment: "Bitcoin (BTC)", amount: "+$10.00" },
   ];
 
   return (
@@ -30,14 +30,14 @@ const TransactionPage = () => {
           {transactions.map((tx) => (
             <tr key={tx.order}>
               <td>{tx.order}</td>
+              <td className={s.amount}>{tx.amount}</td>
               <td>{tx.date}</td>
+              <td>{tx.payment}</td>
               <td>
                 <AiOutlineCheckCircle className={s.circle} />
                 {tx.status}
               </td>
               <td>{tx.type}</td>
-              <td>{tx.payment}</td>
-              <td className={s.amount}>{tx.amount}</td>
             </tr>
           ))}
         </tbody>
