4a04910927b4194e1c25a9ae0690525ebfda92ec
SohailShafiq1 <sohail.shafiq002@gmail.com>
2025-05-22 03:34:34 +0500

    affiliate bugs resolved:

--- Frontend/src/Pages/AffiliateProgram/AffiliateProgram.jsx
+++ Frontend/src/Pages/AffiliateProgram/AffiliateProgram.jsx
@@ -9,6 +9,7 @@ import styles from "./AffiliateProgram.module.css";
 import { NavLink, Navigate } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
 import { useAffiliateAuth } from "../../Context/AffiliateAuthContext";
+import TeamData from "./components/TeamData/TeamData";
 
 const s = styles;
 
@@ -164,55 +165,7 @@ const AffiliateProgram = () => {
 
       {/* Table Display */}
       <div className={s.tableContainer}>
-        {activeTable === "traders" ? (
-          <table className={s.dataTable}>
-            <thead>
-              <tr>
-                <th>TRADER</th>
-                <th>LINK ID</th>
-                <th>BALANCE</th>
-                <th>DEPOSITS</th>
-                <th>DEPOSITS SUM</th>
-                <th>BONUSES</th>
-                <th>WITHDRAWALS</th>
-              </tr>
-            </thead>
-            <tbody>
-              {traders.map((t, i) => (
-                <tr key={i}>
-                  <td>{t.id}</td>
-                  <td className={s.greenLink}>{t.linkId}</td>
-                  <td>{t.balance}</td>
-                  <td>{t.deposits}</td>
-                  <td>{t.depositSum}</td>
-                  <td>{t.bonuses}</td>
-                  <td>{t.withdrawals}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <table className={s.dataTable}>
-            <thead>
-              <tr>
-                <th>TRADER</th>
-                <th>Countries</th>
-                <th>Total Deposit</th>
-                <th>Total Profit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {profits.map((p, i) => (
-                <tr key={i}>
-                  <td>{p.id}</td>
-                  <td>{p.country}</td>
-                  <td>{p.deposit}</td>
-                  <td>{p.profit}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+        <TeamData activeTable={activeTable} traders={traders} profits={profits} />
       </div>
     </div>
   );
