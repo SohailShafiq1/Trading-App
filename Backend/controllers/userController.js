38a341eeb583c41a380c3d1b29d7d1107b277b2d
SohailShafiq1 <sohail.shafiq002@gmail.com>
2025-06-02 03:32:24 +0500

    merge issue resolved

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -457,7 +457,6 @@ export const updateProfile = async (req, res) => {
       ) {
         return res.status(400).json({ error: "CNIC image does not match" });
       }
-    }
     } else if (
       typeof req.body.cnicPicture === "string" &&
       req.body.cnicPicture === ""
